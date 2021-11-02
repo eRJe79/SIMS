f9bcb28f9271158b88915a94de537d3eb26a552e
mathildedc <mathilde.decauna@gmail.com>
2021-11-02 15:19:31 +0100

    Test

--- SIMS/SIMS/views.py
+++ SIMS/SIMS/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from inventory.models import Piece, PieceInstance
+from inventory.models import Piece, PieceInstance, Consumable
 
 
 def index(request):
@@ -23,136 +23,20 @@ def index(request):
 def dashboard(request):
     mypieces = Piece.objects.all()
     my_piece_instances = PieceInstance.objects.all()
-    # We just check the fourth_location attribute since the specific FMS location are at this level
-    fms1_number_seat=0
-    fms1_number_front=0
-    fms1_number_aft=0
-    fms1_number_ofloor=0
-    fms1_number_ufloor=0
-    fms1_number_structure=0
 
-    fms2_number_seat = 0
-    fms2_number_front = 0
-    fms2_number_aft = 0
-    fms2_number_ofloor = 0
-    fms2_number_ufloor = 0
-    fms2_number_structure = 0
+    # Low stock display
+    myconsumables = Consumable.objects.all()
 
-    for piece in my_piece_instances:
-        if piece.second_location == 'FMS1':
-            if piece.fourth_location == 'Seat':
-                fms1_number_seat = fms1_number_seat + 1
-            else:
-                fms1_number_seat = fms1_number_seat
-            if piece.fourth_location == 'Front':
-                fms1_number_front = fms1_number_front + 1
-            else:
-                fms1_number_front = fms1_number_front
-            if piece.fourth_location == 'Aft':
-                fms1_number_aft = fms1_number_aft + 1
-            else:
-                fms1_number_aft = fms1_number_aft
-            if piece.fourth_location == 'Over Floor':
-                fms1_number_ofloor = fms1_number_ofloor + 1
-            else:
-                fms1_number_ofloor = fms1_number_ofloor
-            if piece.fourth_location == 'Under Floor':
-                fms1_number_ufloor = fms1_number_ufloor + 1
-            else:
-                fms1_number_ufloor = fms1_number_ufloor
-            if piece.fourth_location == 'Structure':
-                fms1_number_structure = fms1_number_structure + 1
-            else:
-                fms1_number_structure = fms1_number_structure
-        elif piece.second_location == 'FMS2':
-            if piece.fourth_location == 'Seat':
-                fms2_number_seat = fms2_number_seat + 1
-            else:
-                fms2_number_seat = fms2_number_seat
-            if piece.fourth_location == 'Front':
-                fms2_number_front = fms2_number_front + 1
-            else:
-                fms2_number_front = fms2_number_front
-            if piece.fourth_location == 'Aft':
-                fms2_number_aft = fms2_number_aft + 1
-            else:
-                fms2_number_aft = fms2_number_aft
-            if piece.fourth_location == 'Over Floor':
-                fms2_number_ofloor = fms2_number_ofloor + 1
-            else:
-                fms2_number_ofloor = fms2_number_ofloor
-            if piece.fourth_location == 'Under Floor':
-                fms2_number_ufloor = fms2_number_ufloor + 1
-            else:
-                fms2_number_ufloor = fms2_number_ufloor
-            if piece.fourth_location == 'Structure':
-                fms2_number_structure = fms2_number_structure + 1
-            else:
-                fms2_number_structure = fms2_number_structure
+    consumable_list = []
 
-        if fms1_number_seat > 0:
-            fms1_check_seat = 'OK'
-        else:
-            fms1_check_seat = 'Missing'
-        if fms1_number_front > 0:
-            fms1_check_front = 'OK'
-        else:
-            fms1_check_front = 'Missing'
-        if fms1_number_aft > 0:
-            fms1_check_aft  = 'OK'
-        else:
-            fms1_check_aft  = 'Missing'
-        if fms1_number_ofloor > 0:
-            fms1_check_ofloor = 'OK'
-        else:
-            fms1_check_ofloor = 'Missing'
-        if fms1_number_ufloor > 0:
-            fms1_check_ufloor = 'OK'
-        else:
-            fms1_check_ufloor = 'Missing'
-        if fms1_number_structure > 0:
-            fms1_check_structure = 'OK'
-        else:
-            fms1_check_structure = 'Missing'
+    for item in myconsumables:
+        if item.is_low_stock():
+            consumable_list.append(item)
 
-        if fms2_number_seat > 0:
-            fms2_check_seat = 'OK'
-        else:
-            fms2_check_seat = 'Missing'
-        if fms2_number_front > 0:
-            fms2_check_front = 'OK'
-        else:
-            fms2_check_front = 'Missing'
-        if fms2_number_aft > 0:
-            fms2_check_aft  = 'OK'
-        else:
-            fms2_check_aft  = 'Missing'
-        if fms2_number_ofloor > 0:
-            fms2_check_ofloor = 'OK'
-        else:
-            fms2_check_ofloor = 'Missing'
-        if fms2_number_ufloor > 0:
-            fms2_check_ufloor = 'OK'
-        else:
-            fms2_check_ufloor = 'Missing'
-        if fms1_number_structure > 0:
-            fms2_check_structure = 'OK'
-        else:
-            fms2_check_structure = 'Missing'
+
     context = {
         'mypieces': mypieces,
         'my_piece_instances': my_piece_instances,
-        # 'fms1_check_seat': fms1_check_seat,
-        # 'fms1_check_front': fms1_check_front,
-        # 'fms1_check_aft': fms1_check_aft,
-        # 'fms1_check_ofloor': fms1_check_ofloor,
-        # 'fms1_check_ufloor': fms1_check_ufloor,
-        # 'fms1_check_structure': fms1_check_structure,
-        # 'fms2_check_seat': fms2_check_seat,
-        # 'fms2_check_front': fms2_check_front,
-        # 'fms2_check_aft': fms2_check_aft,
-        # 'fms2_check_ofloor': fms2_check_ofloor,
-        # 'fms2_check_ufloor': fms2_check_ufloor,
-        # 'fms2_check_structure': fms2_check_structure,
+        'consumable_list': consumable_list,
     }
     return render(request, 'dashboard.html', context=context)
